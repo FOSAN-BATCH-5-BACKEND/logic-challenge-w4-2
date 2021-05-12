cf9d200019f9663af5a074a9aaad0199d8949f7e
Rijal Fadhila <r3dlabelz@gmail.com>
2021-05-12 13:35:47 +0700

    dikerjain kalem sambil kayang

---
+++ jmlPrima.js
@@ -0,0 +1,20 @@
+/*
+INSTRUKSI
+=========
+Buatlah sebuah fungsi yang menyatakan jumlah dari tabel bilangan prima yang terdapat pada inputan angka,
+misalnya input adalah 20, maka hasilnya adalah 8,
+karena ada 8 bilangan prima ddi dalam input, yaitu 2,3,5,7,11,13,15,17,19
+
+*/
+
+function howMuch(input) {
+	//kode di sini
+}
+
+
+console.log(howMuch(20)) //8
+console.log(howMuch(30)) //10
+console.log(howMuch(100)) //25
+console.log(howMuch(200)) //46
+console.log(howMuch(1000)) //168
+console.log(howMuch(-5)) //0
---
+++ reverseOrder.js
@@ -0,0 +1,17 @@
+/*
+INSTRUKSI
+=========
+Buatlah fungsi untuk mendapatkan huruf2 unik dalam inputan, namun hasilnya tersusun terbalik
+Contoh:
+Input = "AAAABBBCCDAABBB"
+Output = [ 'B', 'A', 'D', 'C', 'B', 'A' ]
+*/
+
+function reverseOrder(string){
+  // buat kodemu disini
+
+}
+
+console.log(reverseOrder('AAAABBBCCDAABBB')) //[ 'B', 'A', 'D', 'C', 'B', 'A' ]
+console.log(reverseOrder('NNMMAAWWWWWWAAADDDDD')) //[ 'D', 'A', 'W', 'A', 'M', 'N' ]
+console.log(reverseOrder('ABCDEFFFFFFFFFFFFF')) //[ 'F', 'E', 'D', 'C', 'B', 'A' ]
