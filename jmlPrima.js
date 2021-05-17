ff2e280cb65935d34674dc5543bb651653ebf18c
putrihana <putriwijayanti.mtsalmahysar@gmail.com>
2021-05-17 09:24:27 +0700

    tugas rabu, 12 Mei 2021

--- jmlPrima.js
+++ jmlPrima.js
@@ -7,8 +7,37 @@ karena ada 8 bilangan prima ddi dalam input, yaitu 2,3,5,7,11,13,15,17,19
 
 */
 
+
+/*  - mencari data jumlah bilangan prima
+	- bilangan prima adalah bilangan > 1 dan hanya bisa dibagi dengan angka 1 atau bilangan itu sendiri
+	- karena yang dicari adallah jumlah bilangan prima dari angka terdekat dari nilai input sampai yang terakhir dicek di angka 2
+*/
 function howMuch(input) {
-	//kode di sini
+	if (input <= 1) { //karena 1,0, minus bukan bilangan prima
+		return 0;
+	} else if (input == 2) { //karena 2 bilangan prima
+		return 1;
+	} else {
+		let sum = 0;
+		for (let i = 2; i <= input; i++) {
+			// console.log(i);
+
+			// if (input % i !== 0 || (input / 1) === input && input >= 2) {
+			// 	continue;
+
+			let bilanganPrima = true;
+			for (var j = 2; j < i; j++) {
+				if (i % j === 0 && i !== j) {
+					bilanganPrima = false;
+				}
+			}
+			if (bilanganPrima === true) {
+				// console.log(i);
+				sum += 1;
+			}
+		}
+		return `${sum}`;
+	}
 }
 
 
@@ -18,3 +47,4 @@ console.log(howMuch(100)) //25
 console.log(howMuch(200)) //46
 console.log(howMuch(1000)) //168
 console.log(howMuch(-5)) //0
+// console.log(howMuch(2)) //1
--- reverseOrder.js
+++ reverseOrder.js
@@ -7,11 +7,38 @@ Input = "AAAABBBCCDAABBB"
 Output = [ 'B', 'A', 'D', 'C', 'B', 'A' ]
 */
 
-function reverseOrder(string){
-  // buat kodemu disini
+function reverseOrder(string) {
 
+  let array = [];
+
+  let abjad = ['A', 'B', 'C', 'D', 'E', 'F', 'N', 'W', 'M']
+  for (let i = string.length - 1; i >= 0; i--) {
+    array.push(string[i]);
+    for (let j = 0; j < i; j++) {
+      if (string[j] == "AAAA") {
+        // return "A";
+        array += 'A';
+      } if (string[j] == "BBB") {
+        array += 'B';
+      } else if (string[j] == 'CC') {
+        array += 'C';
+      } else if (string[j] == 'DDDDD') {
+        array += 'D';
+      } else if (string[j] == 'FFFFFFFFFFFFF') {
+        array += 'F';
+      } else if (string[j] == 'MM') {
+        array += 'M';
+      } else if (string[j] == 'NN') {
+        array += 'N';
+      } else if (string[j] == 'WW') {
+        array += 'W';
+      }
+    }
+    return array;
+  }
 }
-
+  
+console.log(reverseOrder('AAAA')) //[ 'A' ]
 console.log(reverseOrder('AAAABBBCCDAABBB')) //[ 'B', 'A', 'D', 'C', 'B', 'A' ]
 console.log(reverseOrder('NNMMAAWWWWWWAAADDDDD')) //[ 'D', 'A', 'W', 'A', 'M', 'N' ]
 console.log(reverseOrder('ABCDEFFFFFFFFFFFFF')) //[ 'F', 'E', 'D', 'C', 'B', 'A' ]
