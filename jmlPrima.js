8393596c2f4dd909b047399a6ee26f6c26000618
fariz-bd <budiargof@gmail.com>
2021-06-03 16:50:41 +0700

    gabut

--- jmlPrima.js
+++ jmlPrima.js
@@ -8,13 +8,25 @@ karena ada 8 bilangan prima ddi dalam input, yaitu 2,3,5,7,11,13,15,17,19
 */
 
 function howMuch(input) {
-	//kode di sini
-}
+	let jmlArr = 0;
+    for(let i = 1; i <= input; i++){
+		let penampung = 0;
+		for(let j = 1; j <= i; j++){
+			if(i%j == 0){
+				penampung += 1
+			}
+		}
+		if (penampung == 2){
+			jmlArr+=1
+		}
+    }
+	return jmlArr
+  }
 
 
 console.log(howMuch(20)) //8
-console.log(howMuch(30)) //10
-console.log(howMuch(100)) //25
-console.log(howMuch(200)) //46
-console.log(howMuch(1000)) //168
-console.log(howMuch(-5)) //0
+// console.log(howMuch(30)) //10
+// console.log(howMuch(100)) //25
+// console.log(howMuch(200)) //46
+// console.log(howMuch(1000)) //168
+// console.log(howMuch(-5)) //0
--- reverseOrder.js
+++ reverseOrder.js
@@ -8,7 +8,13 @@ Output = [ 'B', 'A', 'D', 'C', 'B', 'A' ]
 */
 
 function reverseOrder(string){
-  // buat kodemu disini
+ let penampung = []
+  for(let i = string.length-1; i >= 0; i--){
+    if(string[i] !== string[i+1]){
+      penampung.push(string[i])
+    }
+  }
+  return penampung
 
 }
 
